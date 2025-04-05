8f10a59a1e12e07acb50c3a2755590d458c9906b
Philip Mutua <pmutua@live.com>
2025-04-05 07:10:19 +0300

    chore(index.js): Updated main exports
    export { ChatBot } from './components/chat-container.js';
    export { ChatBubble } from './components/chat-bubble.js';
    export { ResizableHandle } from './utils/resizable-handle.js';
    export { defaultConfig } from './utils/config-manager.js';

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-ECHO is on.
+export { ChatBot } from './components/chat-container.js';
+export { ChatBubble } from './components/chat-bubble.js';
+export { ResizableHandle } from './utils/resizable-handle.js';
+export { defaultConfig } from './utils/config-manager.js';
